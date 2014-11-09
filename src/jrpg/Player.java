b79058b21a58e1e6eae00049b30742fef46cb916
Alex (Namone) <al2xnord@yahoo.com>
2014-11-09 13:59:02 -0700

    Some small changes to Player.class

--- src/jrpg/Player.java
+++ src/jrpg/Player.java
@@ -1,22 +1,24 @@
 package jrpg;
 
+import org.lwjgl.util.vector.Vector2f;
+
 public class Player {
     
-    String playerTexture; // Texture object to hold player
+    String playerTexture; // Texture object to hold player sprite
     String playerName;   // Name of player (input by user)
     
     // Player health and player stamina
     private int playerHealth = 100;
     private int playerStamina = 25;
     // Player position on X - Y coordinate 
-    private int playerPositionX, playerPositionY;
+    private Vector2f playerPosition;
     
     
     public void Player(String textureLocation, String name, int positionX, int positionY) {
         
         playerName = name;
         playerTexture = textureLocation;
-        
+        playerPosition = new Vector2f(positionX, positionY);
         LoadPlayerTexture();
         
     }
@@ -24,6 +26,10 @@ public class Player {
     // Load the player texture
     public void LoadPlayerTexture(){
         TextureManager.LoadTexture(playerTexture); // Passed in from Player(String textureLocation)
+    }
+    
+    public void DrawPlayer(){
+        // TODO: PLAYER DRAW CODE GOES HERE
     }
     
     
